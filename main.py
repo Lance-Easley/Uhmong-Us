c1dc4a1c94d9ca74633449dd81d3c741653ff0e2
Lance Easley <leasley21@basecampcodingacademy.org>
2020-11-02 16:08:59 -0600

    Progress

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, pylygon
 pygame.init()
 
 screen_x = 1920
@@ -21,14 +21,17 @@ class Map(object):
     def draw(self, window):
         window.blit(map_image, (self.x, self.y))
 
-    def wall_objs(self):
+    def wall_objs(self, window):
         wall_0 = pygame.Rect(self.x + 2494, self.y + 752, 1562, 472)
-        wall_1 = pygame.Rect(self.x + 2494, self.y + 752, 1562, 472)
-        return [wall_0, wall_1]
+        wall_1 = pygame.Rect(self.x + 3655, self.y + 1402, 401, 480)
+        wall_2 = pygame.Rect(self.x + 2494, self.y + 1402, 904, 480)
+        wall_2 = pygame.Rect(self.x + 2494, self.y + 1402, 904, 480)
+        wall_2 = pygame.transform.rotate(window, 45)
+        return [wall_0, wall_1, wall_2]
     
     def draw_collision(self, window):
-        for wall in self.wall_objs():
-            pygame.draw.rect(window, (0,0,255), wall)
+        for wall in self.wall_objs(window):
+            pygame.draw.rect(window, (0,255,0), wall, 1)
 
 
 class Player(object):
@@ -44,14 +47,14 @@ class Player(object):
 
 def redrawGameWindow():
     skeld.draw(display)
-    #skeld.draw_collision(display)
+    skeld.draw_collision(display)
     p1.draw(display)
     pygame.display.update()
 
 #mainloop
 skeld = Map()
 p1 = Player(screen_x // 2, screen_y // 2, 80, 120)
-collision_tolerance = 5
+collision_tolerance = max(skeld.x_vel, skeld.y_vel) + 1
 run = True
 while run:
     pygame.time.delay(16)
@@ -62,7 +65,7 @@ while run:
     d_coll = True
 
     redrawGameWindow()
-    skeld.wall_objs()
+    skeld.wall_objs(display)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -74,16 +77,12 @@ while run:
     if coll_index != -1:
         if abs(skeld.wall_objs()[coll_index].bottom - p1.hitbox.top) < collision_tolerance:
             w_coll = False
-            print('w')
         if abs(skeld.wall_objs()[coll_index].right - p1.hitbox.left) < collision_tolerance:
             a_coll = False
-            print('a')
         if abs(skeld.wall_objs()[coll_index].top - p1.hitbox.bottom) < collision_tolerance:
             s_coll = False
-            print('s')
         if abs(skeld.wall_objs()[coll_index].left - p1.hitbox.right) < collision_tolerance:
             d_coll = False
-            print('d')
 
     if keys[pygame.K_w]:
         if w_coll:
