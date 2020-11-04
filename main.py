1d2585869f3ffec27386c6b8685d25d8ccf90664
Lance Easley <leasley21@basecampcodingacademy.org>
2020-11-03 20:54:14 -0600

    Added more walls

--- main.py
+++ main.py
@@ -11,26 +11,33 @@ pygame.display.set_caption("Skeld")
 
 class Map(object):
     def __init__(self):
-        self.x = -2600
-        self.y = -1200
+        self.x = -3500
+        self.y = -2200
         self.width = map_image.get_rect().width
         self.height = map_image.get_rect().height
-        self.x_vel = 5
-        self.y_vel = 5
+        self.x_vel = 10
+        self.y_vel = 10
 
     def draw(self, window):
         window.blit(map_image, (self.x, self.y))
 
     def wall_objs(self):
         return [
-            pygame.Rect(self.x + 2494, self.y + 752, 1562, 472), # Top medbay hallway
-            pygame.Rect(self.x + 3655, self.y + 1402, 401, 272), # Bottom right medbay hallway
-            pygame.Rect(self.x + 2494, self.y + 1402, 904, 475), # Bottom left medbay hallway
-            pygame.Rect(self.x + 3710, self.y + 1935, 225, 181), # Bottom right medbay bed
-            pygame.Rect(self.x + 3710, self.y + 1666, 225, 211), # Top right medbay bed
-            pygame.Rect(self.x + 3171, self.y + 1935, 225, 181), # Bottom right medbay bed
-            pygame.Rect(self.x + 3026, self.y + 1880, 145, 920), # Security Left wall
-            pygame.Rect(self.x + 3910, self.y + 1880, 145, 920) # Security Left wall
+            pygame.Rect(self.x + 2494, self.y + 752, 1562, 472), # Medbay: Top hallway
+            pygame.Rect(self.x + 3655, self.y + 1402, 401, 272), # Medbay: Bottom right hallway
+            pygame.Rect(self.x + 2494, self.y + 1402, 904, 475), # Medbay: Bottom left hallway
+            pygame.Rect(self.x + 3710, self.y + 1935, 225, 181), # Medbay: bottom left bed
+            pygame.Rect(self.x + 3710, self.y + 1666, 225, 211), # Medbay: Top right bed
+            pygame.Rect(self.x + 3171, self.y + 1935, 225, 181), # Medbay: Bottom right bed
+            pygame.Rect(self.x + 3917, self.y + 1408, 139, 738), # Medbay: Top left wall
+            pygame.Rect(self.x + 3200, self.y + 2524, 1050, 370), # Medbay: Bottom wall
+            pygame.Rect(self.x + 3026, self.y + 1880, 145, 920), # Security: Right wall
+            pygame.Rect(self.x + 4232, self.y + 2279, 681, 848), # Cafeteria: Bottom right wall
+            pygame.Rect(self.x + 2491, self.y + 2795, 820, 730), # Cafeteria: Bottom right wall
+            pygame.Rect(self.x + 3950, self.y + 2825, 300, 100), # Electrical: top right box
+            pygame.Rect(self.x + 3128, self.y + 2812, 204, 1220), # Electrical: Left wall
+            pygame.Rect(self.x + 3350, self.y + 3170, 440, 200), # Electrical: middle left box
+            pygame.Rect(self.x + 3950, self.y + 2825, 300, 100), # Electrical: top right box
         ]
     
     def draw_collision(self, window):
@@ -66,6 +73,7 @@ def redrawGameWindow():
 skeld = Map()
 p1 = Player(screen_x // 2, screen_y // 2, 80, 120)
 collision_tolerance = max(skeld.x_vel, skeld.y_vel) * 2 + 1
+ghost = False
 run = True
 while run:
     pygame.time.delay(16)
@@ -96,6 +104,13 @@ while run:
             if abs(skeld.wall_objs()[i].left - p1.hitbox.right) < collision_tolerance:
                 d_coll = False
 
+    if keys[pygame.K_x]:
+        ghost = not ghost
+    if ghost:
+        w_coll = True
+        a_coll = True
+        s_coll = True
+        d_coll = True
     if keys[pygame.K_w]:
         if w_coll:
             skeld.y += skeld.y_vel
@@ -108,9 +123,8 @@ while run:
     if keys[pygame.K_d]:
         if d_coll:
             skeld.x -= skeld.x_vel
-    if keys[pygame.K_c]:
-        print(coll_index)
     
+
     if keys[pygame.K_ESCAPE]:
         display = pygame.display.set_mode((screen_x, screen_y))
         pygame.quit()
