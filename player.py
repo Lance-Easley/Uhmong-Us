c8dd49d0611d6eedd8874c275ea6190253936fe4
Lance Easley <leasley21@basecampcodingacademy.org>
2020-12-16 15:48:38 -0600

    Adjusted player hitbox

--- player.py
+++ player.py
@@ -5,14 +5,14 @@ class Player(object):
         self.x = x
         self.y = y
         self.width = 52
-        self.height = 80
+        self.height = 82
         self.color = color
-        self.hitbox = pygame.Rect(self.x, self.y + (self.height // 3) * 2, self.width, self.height // 3)
+        self.hitbox = pygame.Rect(self.x - 2, self.y - 2, self.width + 2, self.height + 2)
         self.x_coord = map_obj.x
         self.y_coord = map_obj.y
 
     def draw(self, window):
-        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(window, self.color, (self.x + 1, self.y + 1, self.width - 2, self.height - 2))
     
     def convert_to_data(self):
         return self.x_coord, self.y_coord, self.color
