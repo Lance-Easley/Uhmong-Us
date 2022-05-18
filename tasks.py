62307bd9f61423f2b320cb911d6c94fa123e6ed8
Lance Easley <josephlanceeasley@gmail.com>
2022-05-18 17:28:41 -0500

    Added Refill Hand Sanitizer task

--- tasks.py
+++ tasks.py
@@ -850,14 +850,25 @@ class RefillHandSanitizer(BaseTask):
         self.new_sanitizer = pygame.image.load('images/tasks/refill_hand_sanitizer/new_sanitizer.png').convert()
         self.old_sanitizer = pygame.image.load('images/tasks/refill_hand_sanitizer/old_sanitizer.png').convert()
         self.unlatched_buckle = pygame.image.load('images/tasks/refill_hand_sanitizer/unlatched_buckle.png').convert()
+        self.up_arrow_hover = pygame.image.load('images/tasks/refill_hand_sanitizer/pull_up_arrow_hover.png').convert()
+        self.up_arrow = pygame.image.load('images/tasks/refill_hand_sanitizer/pull_up_arrow.png').convert()
 
         self.wall.set_colorkey((255, 255, 255))
         self.buckle.set_colorkey((255, 255, 255))
         self.new_sanitizer.set_colorkey((255, 255, 255))
         self.old_sanitizer.set_colorkey((255, 255, 255))
         self.unlatched_buckle.set_colorkey((255, 255, 255))
+        self.up_arrow_hover.set_colorkey((255, 255, 255))
+        self.up_arrow.set_colorkey((255, 255, 255))
 
-        self.unlock_rect = pygame.Rect(745, 557, 420, 60)
+        self.lock_rect = pygame.Rect(920, 782, 80, 90)
+        self.unlock_rect = pygame.Rect(920, 682, 80, 100)
+        self.grab_old_sanitizer_rect = pygame.Rect(830, 382, 260, 394)
+        self.sanitizer_lifted_rect = pygame.Rect(855, 180, 200, 127)
+        self.grab_new_sanitizer_rect = pygame.Rect(500, 486, 260, 394)
+
+        # difference between an object's coords and the mouse position
+        self.grab_offset = (0, 0)
 
         self.task_phase = 0
 
@@ -873,34 +884,117 @@ class RefillHandSanitizer(BaseTask):
         mouse_x, mouse_y = pygame.mouse.get_pos()
 
         self.display.blit(self.wall, (self.x - 480, self.y - 360))
-        self.display.blit(self.buckle, (self.x - 130, self.y + 142))
 
-        # if self.task_phase < 2:
-        #     if pygame.mouse.get_pressed(3)[0]:
-        #         if mouse_over_bag_rect:
-        #             self.task_phase = 1
-        #
-        #         if self.task_phase == 1:
-        #             # Hover Highlight
-        #             if mouse_over_trash_lifted_rect:
-        #                 self.display.blit(self.pull_up_arrow_hover,
-        #                                   (self.trash_lifted_rect.x, self.trash_lifted_rect.y + 20))
-        #             else:
-        #                 self.display.blit(self.pull_up_arrow, (self.trash_lifted_rect.x, self.trash_lifted_rect.y + 20))
-        #
-        #             self.display.blit(self.sacked_bag, (mouse_x - 175, mouse_y))
-        #     else:
-        #         if mouse_over_trash_lifted_rect and self.task_phase == 1:
-        #             self.task_phase = 2
-        #         else:
-        #             self.task_phase = 0
-        #
-        # elif self.task_phase < 4:
-        #     if pygame.mouse.get_pressed(3)[0]:
-        #         if self.new_bag_rect.collidepoint(mouse_x, mouse_y):
-        #             self.task_phase = 3
-        #     else:
-        #         if mouse_over_bag_rect and self.task_phase == 3:
-        #             self.task_phase = 4
-        #         else:
-        #             self.task_phase = 2
+        if self.task_phase < 3:
+            self.display.blit(self.old_sanitizer, (self.x - 130, self.y - 158))
+        elif self.task_phase == 4:
+            self.display.blit(self.new_sanitizer, (500, 486))
+        elif self.task_phase > 5:
+            self.display.blit(self.new_sanitizer, (self.x - 130, self.y - 158))
+
+        if self.task_phase < 2 or self.task_phase == 8:
+            self.display.blit(self.buckle, (self.x - 130, self.y + 142))
+        else:
+            self.display.blit(self.unlatched_buckle, (self.x - 130, self.y + 232))
+
+        mouse_over_buckle = self.unlock_rect.collidepoint(mouse_x, mouse_y)
+        mouse_over_sanitizer_slot = self.grab_old_sanitizer_rect.collidepoint(mouse_x, mouse_y)
+        mouse_over_up_arrow = self.sanitizer_lifted_rect.collidepoint(mouse_x, mouse_y)
+        mouse_over_new_sanitizer = self.grab_new_sanitizer_rect.collidepoint(mouse_x, mouse_y)
+        mouse_over_unlocked_buckle = self.lock_rect.collidepoint(mouse_x, mouse_y)
+
+        # pygame.draw.rect(self.display, (255, 255, 0), self.lock_rect, 3)
+
+        # Phase 1 & 2: Sanitizer unlocked
+        # Phase 3 & 4: Take out old sanitizer
+        # Phase 5 & 6: Put in new sanitizer
+        # Phase 7 & 8: Lock & Task Complete
+
+        if self.task_phase < 2:
+            if pygame.mouse.get_pressed(3)[0]:
+                if mouse_over_buckle:
+                    self.task_phase = 1
+            else:
+                # wait for mouse up before continuing
+                if self.task_phase == 1:
+                    self.task_phase = 2
+
+            # Hover Highlight
+            if mouse_over_buckle:
+                pygame.draw.rect(self.display, (254, 254, 254), self.unlock_rect, 5)
+            else:
+                pygame.draw.rect(self.display, (255, 255, 0), self.unlock_rect, 5)
+
+        elif self.task_phase < 4:
+            if pygame.mouse.get_pressed(3)[0]:
+                if mouse_over_sanitizer_slot:
+                    if self.task_phase == 2:
+                        self.grab_offset = (830 - mouse_x, 382 - mouse_y)
+                        self.task_phase = 3
+
+                if self.task_phase == 3:
+                    # Hover Highlight
+                    if mouse_over_up_arrow:
+                        self.display.blit(self.up_arrow_hover,
+                                          (self.sanitizer_lifted_rect.x, self.sanitizer_lifted_rect.y))
+                    else:
+                        self.display.blit(self.up_arrow,
+                                          (self.sanitizer_lifted_rect.x, self.sanitizer_lifted_rect.y))
+            else:
+                if self.task_phase == 3 and mouse_over_up_arrow:
+                    self.task_phase = 4
+                else:
+                    self.task_phase = 2
+
+                    # Hover Highlight
+                    if mouse_over_sanitizer_slot:
+                        pygame.draw.rect(self.display, (255, 255, 0), self.grab_old_sanitizer_rect, 5)
+                    else:
+                        pygame.draw.rect(self.display, (254, 254, 254), self.grab_old_sanitizer_rect, 5)
+
+        elif self.task_phase < 6:
+            if pygame.mouse.get_pressed(3)[0]:
+                if mouse_over_new_sanitizer:
+                    if self.task_phase == 4:
+                        self.grab_offset = (500 - mouse_x, 486 - mouse_y)
+                        self.task_phase = 5
+
+                if self.task_phase == 5:
+                    # Hover Highlight
+                    if mouse_over_new_sanitizer:
+                        pygame.draw.rect(self.display, (255, 255, 0), self.grab_old_sanitizer_rect, 5)
+                    else:
+                        pygame.draw.rect(self.display, (254, 254, 254), self.grab_old_sanitizer_rect, 5)
+            else:
+                if self.task_phase == 5 and mouse_over_sanitizer_slot:
+                    self.task_phase = 6
+                else:
+                    self.task_phase = 4
+
+                    # Hover Highlight
+                    if mouse_over_new_sanitizer:
+                        pygame.draw.rect(self.display, (255, 255, 0), self.grab_new_sanitizer_rect, 5)
+                    else:
+                        pygame.draw.rect(self.display, (254, 254, 254), self.grab_new_sanitizer_rect, 5)
+        elif self.task_phase < 8:
+            if pygame.mouse.get_pressed(3)[0]:
+                if mouse_over_unlocked_buckle:
+                    self.task_phase = 7
+            else:
+                # wait for mouse up before continuing
+                if self.task_phase == 7:
+                    self.task_phase = 8
+                else:
+                    # Hover Highlight
+                    if mouse_over_unlocked_buckle:
+                        pygame.draw.rect(self.display, (254, 254, 254), self.lock_rect, 5)
+                    else:
+                        pygame.draw.rect(self.display, (255, 255, 0), self.lock_rect, 5)
+
+        # Draw over all (drag phases & success)
+        if self.task_phase == 3:
+            self.display.blit(self.old_sanitizer, (self.grab_offset[0] + mouse_x, self.grab_offset[1] + mouse_y))
+        elif self.task_phase == 5:
+            self.display.blit(self.new_sanitizer, (self.grab_offset[0] + mouse_x, self.grab_offset[1] + mouse_y))
+        elif self.task_phase == 8:
+            return self.success(dt)
